f17b898b442aeae8fb1735a194a22d238c94f302
anas <aguswahyuannasir@gmail.com>
2020-05-12 12:15:26 +0700

    udpate index.php

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+include "phpqrcode/qrlib.php"; 
+
+$codeContents = '202005121034051681'; 
+
+$tempdir = "temp/";
+
+QRcode::png($codeContents,$tempdir."001.png"); 
+?>
+
+<center>
+<table class="m_140125597400510068main-cell" border="0" cellpadding="0" cellspacing="0" style="background-color:#dddddd">
+	<tbody><tr>
+		<td style="background-color:#232323;padding:40px 10px 20px 10px">
+			
+			<center>
+				<img src="https://ci3.googleusercontent.com/proxy/rZIv8lvbTCU5IDN1rJxNhBiqcirqq_uAeorvxUcwt9MAroYaMNEV_4cdpvPBgxwNduptSfSlh7qB7pRa3HpZ8-Owy076ylECBhbZ=s0-d-e1-ft#https://www.spesolution.com/images/layouts/main/logo.png" class="CToWUd">
+			</center>
+		</td>
+	</tr>
+	<tr>
+		<td style="background-color:#121212;font-weight:bold;letter-spacing:1px;border-bottom:5px solid #ec145d;padding:5px">
+			<center>
+				<span style="font-size:11px;color:#ffffff;letter-spacing:0.5px">w w w . s p e s o l u t i o n . c o m</span>
+			</center>
+		</td>
+	</tr>
+	<tr>
+		<td style="background-color:#cccccc;padding:25px;color:#000000;font-size:12px;letter-spacing:0.3px">
+			<table border="0" cellpadding="0" cellspacing="0" width="100%">
+				<tbody><tr>
+					<td style="padding-bottom:25px">
+						<center>
+							Hi Agus Wahyu Annasir, thank you for apply.<br>
+							<br>
+							<br>
+							Warmest greeting for you guys.
+							<br>
+							We are always looking for talented and passionate people to join us in our on-going journey.
+							<br>
+							Goodluck for your trial!
+							<br>
+						</center>
+					</td>
+				</tr>
+				<tr>
+					<td style="padding-top:30px;padding-bottom:25px">
+						<center>
+							<!-- <img src="https://ci6.googleusercontent.com/proxy/yHq5UoTUMc28iQqvwWQZfLYb1A1OzvphM_5W0_1E14whUr4rEbP0bNoiDToEl36rpBT2YrYjPbSc27l6UPID3pYpr7s-UCMcYhThvyFLkcXpPzDepRO9qrlUUupBOkHFwROU-qLkBgtGq7dDVlQ=s0-d-e1-ft#https://www.spesolution.com/uploads/images/recruitment/nxih9u4z4ug-h76wjoi6ullmx7vjgoe8.png" class="CToWUd"> -->
+							<?php
+								echo '<img src="'.$tempdir.'001.png" class="CToWUd">';
+							?>
+						</center>
+					</td>
+				</tr>
+			</tbody></table>
+		</td>
+	</tr>
+	<tr>
+		<td style="background-color:#454545;padding:25px 10px;color:#ffffff;font-size:13px;letter-spacing:0.3px;border-top:5px solid #777777;border-bottom:5px solid #777777">
+			<center>
+				Copyright 2020 | SPE Solution
+				<br>
+				All rights reserved
+			</center>
+		</td>
+	</tr>
+	</tbody>
+</table>
+</center>
